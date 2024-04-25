297be6beeacf71f25c92e7812a8d835d4874b56e
banditVedant <vedant@payatu.io>
2024-04-25 13:56:26 +0530

    Initial commit

---
+++ app/src/main/java/com/ved/taskhijackingdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ved.taskhijackingdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        setContentView(R.layout.activity_main);
+    }
+}
